a8608627aa91bcb9220ce5b317b0e579d5e1564f
William Kc <williamkohinc@gmail.com>
2024-12-11 14:17:44 +0700

    fix treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,30 +1,29 @@
+require("nvim-treesitter.install").compilers = { "zig" }
+
 return {
   -- add more treesitter parsers
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = function()
-      require("nvim-treesitter.install").compilers = { "zig" }
-      return {
-        ensure_installed = {
-          "bash",
-          "html",
-          "javascript",
-          "json",
-          "lua",
-          "markdown",
-          "markdown_inline",
-          "python",
-          "query",
-          "regex",
-          "tsx",
-          "typescript",
-          "vim",
-          "yaml",
-          "php",
-          "go",
-          "sql",
-        },
-      }
-    end,
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+        "php",
+        "go",
+        "sql",
+      },
+    },
   },
 }

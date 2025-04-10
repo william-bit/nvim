fdf9ebc72d84f1d14bdfc15ad8819557c12ad73b
WilliamKc <william@dartmedia.biz>
2025-04-10 15:48:10 +0700

    refractor init.lua

--- init.lua
+++ init.lua
@@ -3,19 +3,10 @@ vim.g.base46_cache = vim.fn.stdpath("data") .. "\\base46_cache\\"
 
 require("configs.lazy")
 
--- put this in your main init.lua file ( before lazy setup )
-vim.g.base46_cache = vim.fn.stdpath("data") .. "/base46_cache/"
-
 -- put this after lazy setup
 
--- (method 1, For heavy lazyloaders)
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
-
--- (method 2, for non lazyloaders) to load all highlights at once
-for _, v in ipairs(vim.fn.readdir(vim.g.base46_cache)) do
-	dofile(vim.g.base46_cache .. v)
-end
 
 require("options")
 require("autocmds")

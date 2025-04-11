f9ad480d1066903693923e4e84c55a74284c4790
WilliamKc <william@dartmedia.biz>
2025-04-11 11:11:24 +0700

    refractor init lua and add csspell directory

--- init.lua
+++ init.lua
@@ -1,7 +1,36 @@
 -- put this in your main init.lua file ( before lazy setup )
 vim.g.base46_cache = vim.fn.stdpath("data") .. "\\base46_cache\\"
 
-require("configs.lazy")
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+-- Setup lazy.nvim
+local lazy_config = require "configs.lazy"
+
+-- load plugins
+require("lazy").setup(lazy_config)
 
 -- put this after lazy setup
 
@@ -15,4 +44,3 @@ require("commands")
 vim.schedule(function()
   require("mappings")
 end)
-

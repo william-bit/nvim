720309565c7707edd705b0b7ee506f76c56f57ee
WilliamKc <william@dartmedia.biz>
2025-04-28 17:27:50 +0700

    add base46 cache

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -34,26 +34,29 @@ return {
           mode = { "n", "t" },
         })
       end
-      return keys;
+      return keys
     end,
-    opts = {
-      shade_terminals = false,
-      shade_filetypes = {},
-      hide_numbers = true,
-      insert_mappings = true,
-      terminal_mappings = true,
-      start_in_insert = true,
-      close_on_exit = true,
-      float_opts = {
-        -- The border key is *almost* the same as 'nvim_open_win'
-        -- see :h nvim_open_win for details on borders however
-        -- the 'curved' border is a custom border type
-        -- not natively supported but implemented in this plugin.
-        border = "curved",
-        -- like `size`, width, height, row, and col can be a number or function which is passed the current terminal
-        winblend = 4,
-        title_pos = "left",
-      },
-    },
+    opts = function()
+      dofile(vim.g.base46_cache .. "term")
+      return {
+        shade_terminals = false,
+        shade_filetypes = {},
+        hide_numbers = true,
+        insert_mappings = true,
+        terminal_mappings = true,
+        start_in_insert = true,
+        close_on_exit = true,
+        float_opts = {
+          -- The border key is *almost* the same as 'nvim_open_win'
+          -- see :h nvim_open_win for details on borders however
+          -- the 'curved' border is a custom border type
+          -- not natively supported but implemented in this plugin.
+          border = "curved",
+          -- like `size`, width, height, row, and col can be a number or function which is passed the current terminal
+          winblend = 4,
+          title_pos = "left",
+        },
+      }
+    end,
   },
 }

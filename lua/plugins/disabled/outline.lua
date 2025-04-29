a1f0dbfb79ab126f45b940c21348d2464659ddc7
WilliamKc <william@dartmedia.biz>
2025-04-29 16:35:24 +0700

    remove outline.nvim reduntant same functionality to trouble.nvim

--- lua/plugins/disabled/outline.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "hedyhli/outline.nvim",
-    cmd = { "Outline", "OutlineOpen" },
-    keys = { -- Example mapping to toggle outline
-      { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
-    },
-  },
-}

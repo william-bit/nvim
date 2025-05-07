b7261be0e2811af19926f30fed2931132bd5011e
WilliamKc <william@dartmedia.biz>
2025-05-08 00:22:06 +0700

    update lspconfig

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -2,42 +2,33 @@ local lsp = require "configs.lsp.default"
 
 lsp.defaults()
 
-vim.lsp.config.lua_ls = {
-  on_attach = lsp.on_attach,
-  capabilities = lsp.capabilities,
-  on_init = lsp.on_init,
-
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using
-        -- (most likely LuaJIT in the case of Neovim)
-        version = "LuaJIT",
-      },
-      diagnostics = {
-        globals = { "vim" },
-      },
-      workspace = {
-        maxPreload = 100000,
-        preloadFileSize = 10000,
-        -- Do not send telemetry data containing a randomized but unique identifier
-        telemetry = {
-          enable = false,
-        },
+vim.lsp.config.lua_ls.settings = {
+  Lua = {
+    runtime = {
+      -- Tell the language server which version of Lua you're using
+      -- (most likely LuaJIT in the case of Neovim)
+      version = "LuaJIT",
+    },
+    diagnostics = {
+      globals = { "vim" },
+    },
+    workspace = {
+      maxPreload = 100000,
+      preloadFileSize = 10000,
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
       },
     },
   },
 }
 
 vim.lsp.config.gradle_ls = {
-  on_attach = lsp.on_attach,
-  on_init = lsp.on_init,
-  capabilities = lsp.capabilities,
   filetypes = { "groovy" },
-  root_dir = require("lspconfig.util").root_pattern(
+  root_markers = {
     "settings.gradle", -- Gradle (multi-project)
-    "build.gradle" -- Gradle
-  ),
+    "build.gradle", -- Gradle
+  },
   cmd = { "gradle-language-server" },
   -- gradle-language-server expects init_options.settings to be defined
   init_options = {
